95c2922d90b9afffef4aa51fef5374d1998431bc
Alvaro Rodrigo Moreira <alvarorodrigo001@gmail.com>
2026-03-09 20:39:39 -0300

    Configuração para GitHub Pages
    Configuração para GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
 const nextConfig = {
+  output: 'export',
+  basePath: basePath,
+  assetPrefix: basePath ? `${basePath}/` : '',
   typescript: {
     ignoreBuildErrors: true,
   },
